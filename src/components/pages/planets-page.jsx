e7a8520283368ac1166e7fc66255d5259d762069
RinatSa <sadykov.r@seznam.cz>
2023-06-08 23:30:34 +0200

    Add: add another two pages components Update: update url props in people-page

--- src/components/pages/planets-page.jsx
+++ src/components/pages/planets-page.jsx
@@ -1,12 +1,12 @@
 import List from "../list.jsx";
 import Info from "../info.jsx";
 
-function PeoplePage() {
+function PlanetsPage() {
     return (
         <div className="row justify-content-between">
-            <List url={`https://swapi.dev/api/people/`}/>
+            <List url={`https://swapi.dev/api/planets/`}/>
             <Info/>
         </div>);
 }
 
-export default PeoplePage;
+export default PlanetsPage;
--- src/components/pages/starships-page.jsx
+++ src/components/pages/starships-page.jsx
@@ -1,12 +1,12 @@
 import List from "../list.jsx";
 import Info from "../info.jsx";
 
-function PeoplePage() {
+function StarshipsPage() {
     return (
         <div className="row justify-content-between">
-            <List url={`https://swapi.dev/api/people/`}/>
+            <List url={`https://swapi.dev/api/starships/`}/>
             <Info/>
         </div>);
 }
 
-export default PeoplePage;
+export default StarshipsPage;
