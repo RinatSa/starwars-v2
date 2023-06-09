b226bcd309745949c5e42fe4a85890bc3acacf3f
RinatSa <sadykov.r@seznam.cz>
2023-06-09 13:20:28 +0200

    Update: add logic and fetch api

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,12 +1,28 @@
-function Info() {
+/* eslint-disable react/prop-types */
+import {useEffect, useState} from "react";
+
+function Info({chosenItem, imgUrl, url}) {
+
+    const [infoData, setInfoData] = useState({})
+
+    useEffect(() => {
+        getInfoData()
+    }, [chosenItem])
+
+    const getInfoData = async () => {
+        const res = await fetch(url + chosenItem)
+        res.json()
+            .then(data => setInfoData(data))
+    }
+
     return (
         <div className="col-5">
             <div>
-                <img src="https://starwars-visualguide.com/assets/img/starships/13.jpg" alt=""
-                     className="planet"/>
+                <img src={`https://starwars-visualguide.com/assets/img/${imgUrl}/${chosenItem}.jpg`} alt=""
+                     className="person"/>
             </div>
             <div>
-                <h1>Endor</h1>
+                <h1>{infoData.name}</h1>
                 <p>Population: 216451314354</p>
                 <p>Rotation Period: 15</p>
                 <p>Diametr: 5646546 km</p>
